e517f62f323112b8a64077ff67e901536a82209e
Caleb Paul and Sandro Alvarez <sandro@>
2016-08-18 09:34:12 -0700

    add for loops in js file that convert input to numerals L, X, V, and I

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function romanConverter(number) {
-  var romArray = [1000,500,100,50,10,5];
+  var romArray = [1000,500,100,50,10,5,1];
   var romNum = ""
   if(number < 1 || number > 3999) {
     return "Enter a number between 1 and 3999!"
@@ -9,13 +9,35 @@ function romanConverter(number) {
       for (var j = 0; j < Math.floor(number/romArray[i]); j++) {
         romNum += "M";
       }
-    } else if (romArray[i] === 500) {
+    }
+    else if (romArray[i] === 500) {
       for (var j = 0; j < Math.floor((number % 1000) /romArray[i]); j++) {
         romNum += "D";
       }
-    } else if (romArray[i] === 100) {
+    }
+    else if (romArray[i] === 100) {
       for (var j = 0; j < Math.floor((number % 500) / romArray[i]); j++) {
         romNum += "C";
+      }
+    }
+    else if (romArray[i] === 50) {
+      for (var j = 0; j < Math.floor((number % 100) / romArray[i]); j++) {
+        romNum += "L";
+      }
+    }
+    else if (romArray[i] === 10) {
+      for (var j = 0; j < Math.floor((number % 50) / romArray[i]); j++) {
+        romNum += "X";
+      }
+    }
+    else if (romArray[i] === 5) {
+      for (var j = 0; j < Math.floor((number % 10) / romArray[i]); j++) {
+        romNum += "V";
+      }
+    }
+    else {
+      for (var j = 0; j < Math.floor((number % 5) / romArray[i]); j++) {
+        romNum += "I";
       }
     }
   }
