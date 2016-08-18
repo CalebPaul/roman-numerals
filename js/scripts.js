ad749fc05ce329895311bd485f1ba27b716ce460
Caleb Paul and Sandro Alvarez <caleb@>
2016-08-18 09:26:11 -0700

    add for loops in js file that convert input to numerals M, D, and C

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
 function romanConverter(number) {
-  var romArray = [];
+  var romArray = [1000,500,100,50,10,5];
+  var romNum = ""
   if(number < 1 || number > 3999) {
     return "Enter a number between 1 and 3999!"
   }
-  for (var i = 0; i < number; i++) {
-    romArray.push("I");
-    alert(romArray);
+  for (var i = 0; i < romArray.length; i++) {
+    if(romArray[i] === 1000) {
+      for (var j = 0; j < Math.floor(number/romArray[i]); j++) {
+        romNum += "M";
+      }
+    } else if (romArray[i] === 500) {
+      for (var j = 0; j < Math.floor((number % 1000) /romArray[i]); j++) {
+        romNum += "D";
+      }
+    } else if (romArray[i] === 100) {
+      for (var j = 0; j < Math.floor((number % 500) / romArray[i]); j++) {
+        romNum += "C";
+      }
+    }
   }
-  return romArray;
+    number % romArray[i];
+
+
+
+  return romNum
 }
 $(document).ready(function() {
   $("form").submit(function(event){
