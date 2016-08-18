8aa9c6c64def4af1b924513ece4e303cfb2cef32
Caleb Paul and Sandro Alvarez <sandro@>
2016-08-18 08:58:39 -0700

    add form, button, output div in html , and add number range alert, and function that returns an array when given numerical input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-$( document ).ready(function() {
+function romanConverter(number) {
+  var romArray = [];
+  if(number < 1 || number > 3999) {
+    return "Enter a number between 1 and 3999!"
+  }
+  for (var i = 0; i < number; i++) {
+    romArray.push("I");
+    alert(romArray);
+  }
+  return romArray;
+}
+$(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
 
+    var startNum = $("#input-number").val();
+    var output = romanConverter(startNum);
+    $("#output").text(output);
+  });
 });
